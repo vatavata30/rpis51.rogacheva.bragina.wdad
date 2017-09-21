71ec98d116d007647b1fd9faffd622c39c34c0a7
ElenaAbroskina <32158143+ElenaAbroskina@users.noreply.github.com>
2017-09-21 14:06:31 +0400

    Added entry point

---
+++ src/Application.java
@@ -0,0 +1,8 @@
+/**
+ * Created by U_LIVT35414 on 21.09.2017.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("we are Rogacheva and Bragina, and we are not a monkeys");
+    }
+}
